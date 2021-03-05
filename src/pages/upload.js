4f005af1ed3b08e66dab46b612cf51208f4d4360
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-03-05 22:39:38 +0100

    Remove useEffect warning

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -42,7 +42,7 @@ export default function UploadPage() {
             clearInterval(timer);
             clearInterval(loadTokenTimer);
         };
-    }, []);
+    }, [enqueueSnackbar]);
 
     return (
         <>
