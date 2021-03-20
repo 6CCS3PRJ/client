32fc87904e7b6e13cfe72a2aa1d25bf152ede93a
Danilo Del Busso <danilo.delbusso1@gmail.com>
2021-03-20 13:08:10 +0100

    Remove getWifi endpoint

--- src/api/server/routes/wifis/get.js
+++ src/api/server/routes/wifis/get.js
@@ -1,11 +1,7 @@
 import { get } from "../../api";
 
-const getWifis = async () => {
-  return await get("/wifis/get/features"); //todo: should be a separate endpoint
-};
-
 const getFeatures = async () => {
   return await get("/wifis/get/features");
 };
 
-export { getWifis, getFeatures };
+export { getFeatures };
